2c64ae184a67b5e30213b7a76b0e3a9ab6866c7f
lwamuhaji <lwamuhaji@gmail.com>
2022-04-14 22:11:32 +0900

    feat: DM 화면 구현

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,8 +1,30 @@
 import Workspace from '@layouts/Workspace';
+import { Container, Header } from '@pages/Channel/styles';
 import React from 'react';
+import gravatar from 'gravatar';
+import { useParams } from 'react-router';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
 
 const DirectMessage = () => {
-  return <div>나는야 디엠</div>;
+  const { workspace, id } = useParams();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      {/* <ChatList/>
+    <ChatBox/> */}
+    </Container>
+  );
 };
 
 export default DirectMessage;
